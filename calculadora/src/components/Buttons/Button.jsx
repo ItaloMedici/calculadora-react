c3d7eecbc005706fbf8822c728c59fd3dbbcd2a5
ItaloMedici <italomedice969@gmail.com>
2021-06-06 23:21:42 -0300

    update buttuns styles and props

--- calculadora/src/components/Buttons/Button.jsx
+++ calculadora/src/components/Buttons/Button.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import './button.scss';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default props => 
-    <button className='button'>{props.label}</button>
+export default props => {
+  let classes = 'button ';
+  classes += props.operation ? 'operation ' : '';
+  classes += props.double ? 'double ' : '';
+  classes += props.triple ? 'triple ' : '';
+
+  return (
+    <button 
+      className={classes}
+      onClick={event => props.click && props.click(props.label)}
+    >
+      {props.label}
+    </button>
+  )
+}
 
