76d75a1091afe72fa32bfc4a510f5659bb77f28c
ItaloMedici <italomedice969@gmail.com>
2021-06-07 21:12:26 -0300

    add math functions to calculte the values

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -4,32 +4,90 @@ import './calculadora.scss'
 import Button from '../components/Buttons/Button'
 import Display from '../components/Display/Display'
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0
+}
+
 export default class Calculator extends Component {
+
+  state = {...initialState};
 
   constructor(props) {
     super(props);
     this.setOperation = this.setOperation.bind(this);
     this.addDigit = this.addDigit.bind(this);
-    this.clear = this.clear.bind(this);
+    this.clearDisplay = this.clearDisplay.bind(this);
   }
 
-  clear() {
-    console.log('limpando');
+  clearDisplay() {
+    this.setState({ ...initialState });
   }
 
   setOperation(operation) {
-    console.log(operation);
+    if(this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const flagEquals = operation === '=';
+      const currentOperation = this.state.operation;
+
+      const values = [...this.state.values];
+      try {
+        switch (currentOperation) {
+          case ("+") : values[0] += values[1]; break;
+          case ("-") : values[0] -= values[1]; break;
+          case ("÷") : values[0] /= values[1]; break;
+          case ("x") : values[0] *= values[1]; break;
+          default: break;
+        }
+      } catch (err) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0;
+
+      this.setState({
+        displayValue: values[0],
+        clearDisplay: !flagEquals,
+        operation: flagEquals ? null : operation,
+        values: values,
+        current: flagEquals ? '0' : '1'
+      })
+    }
   }
 
   addDigit(digit) {
-    console.log(digit);
+    /* Evita adição de 2 pontos em so numero */
+    if(digit === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+    
+    const clearDisplay = this.state.displayValue === "0"
+      || this.state.clearDisplay;
+    const currentValue = clearDisplay ? '' : this.state.displayValue;
+    const displayValue = currentValue + digit;
+
+    this.setState({ displayValue, clearDisplay: false })
+
+    if(digit !== '.') {
+      const idx = this.state.current;
+      const newValue = parseFloat(displayValue);
+      const values = [...this.state.values];
+
+      values[idx] = newValue;
+
+      this.setState({ values });
+    }
   }
 
   render () {
     return (
       <div className="calculadora"> 
-        <Display value="100"/>
-        <Button label="AC" click={this.clear} triple/>
+        <Display value={this.state.displayValue}/>
+        <Button label="AC" click={this.clearDisplay} triple/>
         <Button label="÷" click={this.setOperation} operation/>
         <Button label="7" click={this.addDigit}/>
         <Button label="8" click={this.addDigit}/>
